5ba5e8fb5f9df5b7104a76cda34365292de88950
Lucas Fleischer <lfleischer@itba.edu.ar>
2023-08-15 18:56:41 -0300

    Files generator finished

--- TP2/src/main/java/ar/edu/itba/ss/App.java
+++ TP2/src/main/java/ar/edu/itba/ss/App.java
@@ -1,14 +1,18 @@
 package ar.edu.itba.ss;
 
+import ar.edu.itba.ss.models.Parameters;
+import ar.edu.itba.ss.models.Particle;
+import ar.edu.itba.ss.simulation.OffLatticeSimulation;
+import ar.edu.itba.ss.utils.ParticleGeneration;
 import ar.edu.itba.ss.utils.WriteFiles;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.Locale;
 
 public class App {
     public static void main( String[] args ) throws IOException {
@@ -30,11 +34,43 @@ public class App {
         double eta = configObject.get("eta").getAsDouble();
         double velocity = configObject.get("velocity").getAsDouble();
 
+        // Escribo el archivo static.txt
         WriteFiles writeFiles = new WriteFiles();
         writeFiles.writeFiles(staticFileName, particleRadius, l, n, rc, velocity);
 
+        // En base a la info del archivo static.txt genero las particulas
+        Parameters particles = ParticleGeneration.generateParticles(staticFileName);
 
+        // Inicializo la instancia de la simulacion
+        OffLatticeSimulation offLatticeSimulation = new OffLatticeSimulation(eta, particles, l);
 
+        // Hago la simulacion
+        for (int i = 0; i < iterations; i++) {
+            offLatticeSimulation.group();
+            generateOutputFile(outputFileName, offLatticeSimulation.getParticles(), offLatticeSimulation.getTime());
+        }
 
+    }
+
+    private static void generateOutputFile(String fileName, List<Particle> particles, int time) throws IOException {
+        PrintWriter outputWriter = new PrintWriter(new FileWriter(fileName));
+        StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.append(time).append("\n");
+
+        String formattedLine = null;
+        for(Particle particle : particles) {
+            formattedLine = String.format(Locale.US, "%d %.2f %.2f %.2f %.2f%n",
+                    particle.getId(), particle.getX(),
+                    particle.getY(), particle.getV(),
+                    particle.getTheta());
+            stringBuilder.append(formattedLine);
+        }
+
+        if(formattedLine != null) {
+            outputWriter.write(formattedLine);
+        }
+
+        outputWriter.close();
     }
 }
--- TP2/src/main/java/ar/edu/itba/ss/utils/ParticleGeneration.java
+++ TP2/src/main/java/ar/edu/itba/ss/utils/ParticleGeneration.java
@@ -7,6 +7,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class ParticleGeneration {
     private static Parameters parameters;
@@ -16,21 +17,23 @@ public class ParticleGeneration {
 
         try (BufferedReader reader = new BufferedReader(new FileReader(staticFileName))) {
             String line;
+            AtomicInteger particleId = new AtomicInteger(0);
             //  Itero sobre cada linea del archivo que existe
             while ((line = reader.readLine()) != null) {
                 //  Meto los valores de cada linea en un string
                 String[] values = line.split("\t");
-
-                //  Para cada valor que corresponde a esa linea, lo agarro
-                for (String value : values) {
-                    System.out.print(value + "\t");
+                double[] valuesInDoubles = new double[values.length];
+                for (int i = 0; i < values.length; i++) {
+                    valuesInDoubles[i] = Double.parseDouble(values[i]);
                 }
-                System.out.println();  // Nueva línea después de cada línea del archivo
+                parameters.addParticle(new Particle(particleId.getAndAdd(1), valuesInDoubles[1], valuesInDoubles[2], valuesInDoubles[3], valuesInDoubles[4], valuesInDoubles[0]));
             }
 
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        return parameters;
 
     }
 }
