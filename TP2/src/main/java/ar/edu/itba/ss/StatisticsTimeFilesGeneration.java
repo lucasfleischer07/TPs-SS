c6cb0541ac2115c5abb83cd97f77271cbd0a90f2
MALENA BANFI <mabanfi@itba.edu.ar>
2023-09-05 12:31:07 -0300

    adding config

--- TP2/src/main/java/ar/edu/itba/ss/StatisticsTimeFilesGeneration.java
+++ TP2/src/main/java/ar/edu/itba/ss/StatisticsTimeFilesGeneration.java
@@ -14,8 +14,8 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class StatisticsTimeFilesGeneration {
-    public static double ETA_MAX = 3.5;
-    public static int N_MAX = 800;
+    public static int ETA_MAX = 5;
+    public static int N_MAX = 3600;
     public static void main(String[] args) throws IOException {
         //  Lectura del archivo JSON
         InputStream inputStream = Files.newInputStream(Paths.get("config.json"));
@@ -36,32 +36,10 @@ public class StatisticsTimeFilesGeneration {
         WriteFiles writeFiles = new WriteFiles();
 
 //        * Este for es para el ruido
-        int n = configObject.get("N").getAsInt();
-        for(double eta = 0; eta <= ETA_MAX; eta += 1.5) {
-            if(eta == 3.0) {
-                eta += 0.5;
-            }
-            // Escribo el archivo static.txt
-            writeFiles.generateStaticFile(staticFileName + "Eta" + eta + "N" + n + "L" + (int)l + ".txt", particleRadius, l, n, rc, velocity);
-
-            // En base a la info del archivo static.txt genero las particulas
-            Parameters particles = ParticleGeneration.generateParticles(staticFileName + "Eta" + eta + "N" + n + "L" + (int)l + ".txt");
-
-            // Inicializo la instancia de la simulacion
-            OffLatticeSimulation offLatticeSimulation = new OffLatticeSimulation(eta, particles, l);
-
-            // Hago la simulacion
-            for (int j = 0; j < iterations; j++) {
-                offLatticeSimulation.nextIteration();
-                writeFiles.generateOutputFile(outputFileName + "Eta" + eta + "N" + n + "L" + (int)l + ".txt", offLatticeSimulation.getParticles(), offLatticeSimulation.getTime());
-            }
-        }
-
-//        * Este es para la densidad
-//        double eta = configObject.get("eta").getAsDouble();
-//        for(int n = 100; n <= N_MAX; n += 300) {
-//            if(n == 700) {
-//                n += 100;
+//        int n = configObject.get("N").getAsInt();
+//        for(int eta = 0; eta <= ETA_MAX; eta += 2) {
+//            if(eta == 4) {
+//                eta += 1;
 //            }
 //            // Escribo el archivo static.txt
 //            writeFiles.generateStaticFile(staticFileName + "Eta" + eta + "N" + n + "L" + (int)l + ".txt", particleRadius, l, n, rc, velocity);
@@ -78,6 +56,28 @@ public class StatisticsTimeFilesGeneration {
 //                writeFiles.generateOutputFile(outputFileName + "Eta" + eta + "N" + n + "L" + (int)l + ".txt", offLatticeSimulation.getParticles(), offLatticeSimulation.getTime());
 //            }
 //        }
+
+//        * Este es para la densidad
+        double eta = configObject.get("eta").getAsDouble();
+        for(int n = 200; n <= N_MAX; n += 1400) {
+            if(n == 3000) {
+                n += 600;
+            }
+            // Escribo el archivo static.txt
+            writeFiles.generateStaticFile(staticFileName + "Eta" + eta + "N" + n + "L" + (int)l + ".txt", particleRadius, l, n, rc, velocity);
+
+            // En base a la info del archivo static.txt genero las particulas
+            Parameters particles = ParticleGeneration.generateParticles(staticFileName + "Eta" + eta + "N" + n + "L" + (int)l + ".txt");
+
+            // Inicializo la instancia de la simulacion
+            OffLatticeSimulation offLatticeSimulation = new OffLatticeSimulation(eta, particles, l);
+
+            // Hago la simulacion
+            for (int j = 0; j < iterations; j++) {
+                offLatticeSimulation.nextIteration();
+                writeFiles.generateOutputFile(outputFileName + "Eta" + eta + "N" + n + "L" + (int)l + ".txt", offLatticeSimulation.getParticles(), offLatticeSimulation.getTime());
+            }
+        }
 
     }
 }
