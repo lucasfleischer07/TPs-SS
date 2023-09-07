7c81e48a406936a66da0c66370c99b245caf178a
Lucas Fleischer <lfleischer@itba.edu.ar>
2023-09-07 18:38:39 -0300

    Simulation finished

--- TP3/graphics/main_simulation.py
+++ TP3/graphics/main_simulation.py
@@ -1,23 +1,36 @@
 import matplotlib.pyplot as plt
 from matplotlib import animation
+from matplotlib.collections import EllipseCollection
 
-from graphics.parse_files import parse_config_json
+from graphics.parse_files import parse_config_json, parse_output_file
 
 
-def update_particle_for_simulation(frame, ax2d, main_height, main_width, minor_height, minor_width):
+def update_particle_for_simulation(frame, particle_data, ax2d, main_height, main_width, minor_height, minor_width):
+    # Esta línea obtiene una lista de todas las claves del diccionario events, convierte esa lista en una lista ordenada, y luego selecciona la clave en la posición frame. Esto significa que t ahora contiene la clave correspondiente al paso en la animación que se debe representar en el gráfico.
+    t = list(particle_data.keys())[frame]
+    particles = particle_data[t]
+
     ax2d.clear()
 
     ax2d.plot([0, 0], [0, main_height], color='b')  # 0
     ax2d.plot([0, main_width], [0, 0], color='b')  # 1
     ax2d.plot([main_width, main_width], [0, (main_height - minor_height) / 2], color='b')  # 2
-    ax2d.plot([main_width, main_width + minor_width],[(main_height - minor_height) / 2, (main_height - minor_height) / 2], color='b')  # 3
+    ax2d.plot([main_width, main_width + minor_width], [(main_height - minor_height) / 2, (main_height - minor_height) / 2], color='b')  # 3
     ax2d.plot([main_width + minor_width, main_width + minor_width], [(main_height - minor_height) / 2, (main_height - minor_height) / 2 + minor_height], color='b')  # 4
-    ax2d.plot([main_width + minor_width, main_width],[(main_height - minor_height) / 2 + minor_height, (main_height - minor_height) / 2 + minor_height],color='b')  # 5
-    ax2d.plot([main_width, main_width], [(main_height - minor_height) / 2 + minor_height, main_height],color='b')  # 6
+    ax2d.plot([main_width + minor_width, main_width], [(main_height - minor_height) / 2 + minor_height, (main_height - minor_height) / 2 + minor_height], color='b')  # 5
+    ax2d.plot([main_width, main_width], [(main_height - minor_height) / 2 + minor_height, main_height], color='b')  # 6
     ax2d.plot([main_width, 0], [main_height, main_height], color='b')  # 7
 
     ax2d.set_xticks([0, main_width, main_width + minor_width])
     ax2d.set_yticks([0, (main_height - minor_height) / 2, (main_height - minor_height) / 2 + minor_height, main_height])
+
+    x = [p['x'] for p in particles]  # Se crea una lista x que contiene las coordenadas x de todas las partículas en la lista particles.
+    y = [p['y'] for p in particles]  # Same que x
+    # colors = [p['color'] for p in particles]
+    # diameters = [p['radius'] * 2 for p in particles]  # Se crea una lista diameters que contiene el diámetro (el doble del radio) de todas las partículas en la lista particles.
+
+    ax2d.scatter(([p['x'] for p in particles]), ([p['y'] for p in particles]), s=3*2.0, c='b')
+
 
 def main():
     config_json_path = "../config.json"
@@ -30,9 +43,10 @@ def main():
     ax2d = fig.add_subplot(111)
 
     N, particleRadius, enclosure1X, enclosure1Y, enclosure2X, L = parse_config_json(config_json_path)
+    particle_data = parse_output_file(output_file_path_aux)
 
     # Llama a la función de actualización de la trama
-    anim = animation.FuncAnimation(plt.gcf(), update_particle_for_simulation, fargs=(ax2d, enclosure1Y, enclosure1X, L, enclosure2X), frames=len([1]))
+    anim = animation.FuncAnimation(fig, update_particle_for_simulation, fargs=(particle_data, ax2d, enclosure1Y, enclosure1X, L, enclosure2X), frames=len(particle_data))
 
     # Save animation as mp4
     Writer = animation.writers['ffmpeg']
--- TP3/graphics/parse_files.py
+++ TP3/graphics/parse_files.py
@@ -13,3 +13,33 @@ def parse_config_json(file_path):
         enclosure2Y = config["enclosure2Y"]
 
         return N, particleRadius, enclosure1X, enclosure1Y, enclosure2X, enclosure2Y
+
+
+def parse_output_file(file_path):
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+
+    # Inicializa las variables para almacenar temporalmente los datos
+    particles_data = {}
+    time = None
+
+    for line in lines:
+        # data = line.split("\t")
+        data = line.split()
+
+        if len(data) == 1:
+            time = float(data[0])
+            particles_data[time] = []
+        else:
+            particle = {
+                'x': float(data[0]),
+                'y': float(data[1]),
+                'vx': float(data[2]),
+                'vy': float(data[3]),
+                # 'radius': float(data[4]),
+                # 'color': str(data[5])
+            }
+
+            particles_data[time].append(particle)
+
+    return particles_data
